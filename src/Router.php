13fb80dc9b72220f87b78f9863ddcae22be461f5
Aurélio Soares <asc.mat@gmail.com>
2024-02-28 20:26:13 -0300

    defined router initial structure

--- src/Router.php
+++ src/Router.php
@@ -4,5 +4,36 @@ namespace AurelioSoares\SimpleRouter;
 
 class Router
 {
+    //Path to the classes that will handle the request
+    protected $path;
 
+    protected function setPath(string $path): void 
+    {
+        $this->path = $path;
+    }
+
+    protected function createRoute(
+        string $verb,
+        string $route,
+        string $class,
+        string $method
+    ): void {
+
+    }
+
+    public function get(
+        string $route, 
+        string $class, 
+        string $method
+    ): void {
+        $this->createRoute("GET", $route, $class, $method);
+    }
+
+    public function post(
+        string $route, 
+        string $class, 
+        string $method
+    ): void {
+        $this->createRoute("POST", $route, $class, $method);
+    }
 }
